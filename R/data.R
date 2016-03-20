defd493db1f105134bc72cb84ff4a678d44de0bd
Kushal K Dey <kshldey@gmail.com>
2016-03-20 16:13:25 -0500

    bioccheck and check

--- R/data.R
+++ R/data.R
@@ -1,6 +1,7 @@
 #' Mouse Spleen single cell RNA-seq data due to Jaitin et al (2014)
 #'
-#' A dataset containing single cell RNA-seq data from mouse spleen due to Jaitin et al (2014)
+#' A dataset containing single cell RNA-seq data from mouse spleen due to
+#' Jaitin et al (2014)
 #'
 #' @format Data is an \code{ExpressionSet} object. The
 #' \code{exprs} slot contains the gene read counts in a gene
@@ -9,13 +10,18 @@
 #' including batches, sorting markers, ERCC information etc.\code{featureData}
 #' slot contains row information which are usually ensemebl gene ID or gene
 #' symbols.
+#' @return Returns an ExpressionSet object for the sequencing data
 #' @examples
 #' data(MouseJaitinSpleen)
 #' Biobase::exprs(MouseJaitinSpleen)
 #' Biobase::pData(MouseJaitinSpleen)
 #' Biobase::featureData(MouseJaitinSpleen)
 #' @references
-#' Diego Adhemar Jaitin, Ephraim Kenigsberg, Hadas Keren-Shaul, Naama Elefant, Franziska Paul, Irina Zaretsky, Alexander Mildner, Nadav Cohen, Steffen Jung, Amos Tanay, Ido Amit. Massively Parallel Single-Cell RNA-Seq for Marker-Free Decomposition of Tissues into Cell Types. Science 343, 776-779 (2014) DOI: 10.1126/science.1247651
+#' Diego Adhemar Jaitin, Ephraim Kenigsberg, Hadas Keren-Shaul,
+#' Naama Elefant, Franziska Paul, Irina Zaretsky, Alexander Mildner,
+#' Nadav Cohen, Steffen Jung, Amos Tanay, Ido Amit. Massively Parallel
+#' Single-Cell RNA-Seq for Marker-Free Decomposition of Tissues into Cell Types.
+#' Science 343, 776-779 (2014) DOI: 10.1126/science.1247651
 "MouseJaitinSpleen"
 
 #' Mouse Embryonic stem cell (Esc) RNA-seq data due to Deng et al (2014)
@@ -32,18 +38,23 @@
 #' development stage the cell comes from and the id of the embryo it belongs to.
 #' \code{featureData} slot contains row information which are usually ensemebl
 #' gene ID or gene symbols.
+#' @return Returns an ExpressionSet object for the sequencing data
 #' @examples
 #' data(Deng2014MouseESC)
 #' Biobase::exprs(Deng2014MouseESC)
 #' Biobase::pData(Deng2014MouseESC)
 #' Biobase::featureData(Deng2014MouseESC)
 #' @references
-#' Qiaolin Deng, Daniel Ramsköld, Björn Reinius, Rickard Sandberg. Single-Cell RNA-Seq Reveals Dynamic, Random Monoallelic Gene Expression in Mammalian Cells. Science, Vol. 343, Issue 6167, pp. 193-196, DOI: 10.1126/science.1245316
+#' Qiaolin Deng, Daniel Ramsköld, Björn Reinius, Rickard Sandberg.
+#' Single-Cell RNA-Seq Reveals Dynamic, Random Monoallelic Gene Expression in Mammalian Cells.
+#' Science, Vol. 343, Issue 6167, pp. 193-196, DOI: 10.1126/science.1245316
 "Deng2014MouseESC"
 
-#' Single cell RNA seq data from mouse cortex and hippocampus due to Zeisel et al (2015)
+#' Single cell RNA seq data from mouse cortex and hippocampus due to Zeisel et al
+#' (2015)
 #'
-#' A dataset containing single cell RNA-seq data from mouse cortex and hippocampus
+#' A dataset containing single cell RNA-seq data from mouse cortex and
+#' hippocampus
 #' due to Zeisel et al (2015)
 #'
 #' @format Data is an \code{ExpressionSet} object. The
@@ -55,12 +66,18 @@
 #' of the subjects these cells have been collected from.
 #' \code{featureData} slot contains row information which are usually ensemebl
 #' gene ID or gene symbols.
+#' @return Returns an ExpressionSet object for the sequencing data
 #' @examples
 #' data(MouseZeiselBrain)
 #' Biobase::exprs(MouseZeiselBrain)
 #' Biobase::pData(MouseZeiselBrain)
 #' Biobase::featureData(MouseZeiselBrain)
 #' @references
-#' Amit Zeisel, Ana B Munoz-Manchado, Simone Codeluppi, Peter Lönnerberg, Gioele La Manno, Anna Juréus, Sueli Marques, Hermany Manguba, Liqun He, Christer Betsholtz, Charlott Rolny, Goncalo Castelo-Branco, Jens Hjerling-Leffler, & Sten Linarsson. Cell types in the mouse cortex and hippocampus revleaed by single-cell RNA-seq. Science 34: 6226, 1138-1142 (2015) doi:10.1126/science.aaa1934
+#' Amit Zeisel, Ana B Munoz-Manchado, Simone Codeluppi, Peter Lönnerberg,
+#' Gioele La Manno, Anna Juréus, Sueli Marques, Hermany Manguba, Liqun He,
+#' Christer Betsholtz, Charlott Rolny, Goncalo Castelo-Branco,
+#' Jens Hjerling-Leffler, & Sten Linarsson.
+#' Cell types in the mouse cortex and hippocampus revleaed by single-cell RNA-seq.
+#' Science 34: 6226, 1138-1142 (2015) doi:10.1126/science.aaa1934
 "MouseZeiselBrain"
 
